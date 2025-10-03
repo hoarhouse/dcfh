6b3ec4ca8540a3486467b56988c21ea2fe026281
hoarhouse <previewcapital@gmail.com>
2025-10-03 05:22:03 -0600

    ss

--- scripts/translate-database-content.js
+++ scripts/translate-database-content.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Google Translate API configuration
-const API_KEY = 'AIzaSyDQIDAjqFevGSBLdO6JsPmLyU1bk0nY7bg';
+const API_KEY = 'AIzaSyCEmkwxwJhw1Gcq_CIi0OBekHyuNCC-32U';
 const TRANSLATE_API_URL = 'https://translation.googleapis.com/language/translate/v2';
 
 // Target languages
